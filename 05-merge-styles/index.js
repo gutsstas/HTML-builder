96f15f636dff7ac3f91d966b9cd6e2df792ce104
Stanislav Gutsev <gutsstas@mail.ru>
2022-11-07 22:59:25 +0300

    fix: change function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,13 @@
-const fs = require('fs');
 const fsProm = require('fs/promises');
 const path = require('path');
 
 (async () => {
-  try {
-    const files = await fsProm.readdir(path.join(__dirname, './styles'),{withFileTypes: true});
-    fsProm.writeFile(__dirname + '/project-dist/' + 'bundle.css', '');
-    for (const file of files){
-      if (file.isFile() && path.extname(file.name).slice(1) == 'css' ){
-        fs.readFile(path.join(__dirname, './styles') + '/' + file.name, "utf8", function(error,data){
-          if(error) throw error; // если возникла ошибка
-          fsProm.appendFile(__dirname + '/project-dist/' + 'bundle.css', data);
-        });
+  const files = await fsProm.readdir(path.join(__dirname, './styles'),{withFileTypes: true});
+  await fsProm.writeFile(__dirname + '/project-dist/' + 'bundle.css', '');
+  for (const file of files){
+    if (file.isFile() && path.extname(file.name).slice(1) == 'css' ){
+      const styleFile = await fsProm.readFile(path.join(__dirname, './styles') + '/' + file.name, "utf8");
+      await fsProm.appendFile(__dirname + '/project-dist/' + 'bundle.css', styleFile);
       }
     }
-  } catch (err) {
-    console.error(err);
-  }
 })()
